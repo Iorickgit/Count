a9e1209385e4ba4ccdfa3234f624b22eed6d2e49
Iori Minami <iori.minami@gmail.com>
2016-06-05 13:54:33 +0900

    Initial Commit

---
+++ count/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  count
+//
+//  Created by Minami on 2016/06/05.
+//  Copyright © 2016年 Minami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
